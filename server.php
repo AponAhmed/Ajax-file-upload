76029f1ceac0b94cb8241818a02bc281e58b9e72
AponAhmed <apon2041@gmail.com>
2018-01-05 15:33:16 +0600

    Create server.php

---
+++ server.php
@@ -0,0 +1,12 @@
+//ATTACH define location to move file 
+if (isset($_FILES['ticket_attach'])) {
+    $fname = str_replace(",", "+", $_FILES['ticket_attach']['name']);
+    $fname = str_replace("'", "", $fname);
+    $pinfo = pathinfo($fname);
+    $icn = findIcon("." . $pinfo['extension'], '1x');
+    $fname .= "," . $ufname = md5(time() . $pinfo['filename']) . "." . $pinfo['extension'];
+    $fr = @copy($_FILES['ticket_attach']['tmp_name'], ATTACH . "/$ufname");
+    $error = ob_get_contents();
+    @ob_end_clean();
+    echo json_encode(array("error" => $error, "fname" => $fname, 'icon' => $icn));
+}
